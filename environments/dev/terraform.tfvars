861d808398cd5a3e46c172ebdf0f339eac4c458f
okulab <pamella.mballa@okulab.co>
2020-04-17 23:59:32 +0200

    update gcp project and bucket

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="precise-equator-274319"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "precise-equator-274319-tfstate"
     prefix = "env/prod"
   }
 }
